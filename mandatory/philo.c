ca16724e0ded6f4cdbee17e1b52f08dc57db0319
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-03-10 12:26:13 +0000

    1337

--- mandatory/philo.c
+++ mandatory/philo.c
@@ -6,7 +6,7 @@
 /*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 23:13:07 by houamrha          #+#    #+#             */
-/*   Updated: 2024/03/09 23:15:43 by houamrha         ###   ########.fr       */
+/*   Updated: 2024/03/10 12:26:03 by houamrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,9 @@ int	parse(int argc, char **argv, t_philo *philo)
 	philo->t_eat = ft_atoi(argv[3]);
 	philo->t_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-	{
 		philo->n_must_eat = ft_atoi(argv[5]);
-		if (!philo->n_must_eat)
-			return (0);
-	}
-	if (philo->n_filo > 200 || philo->t_die < 60 || philo->t_eat < 60
+	if (!philo->n_filo ||  philo->n_filo > 200 || philo->t_die < 60 || philo->t_eat < 60
 		|| philo->t_sleep < 60)
-		return (0);
-	if (!philo->n_filo || !philo->t_die || !philo->t_eat || !philo->t_sleep)
 		return (0);
 	return (1);
 }
