55dd8f73fccd551f09e7cba86b1f7bbe064f1e8d
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-03-10 12:23:01 +0000

    1337

--- mandatory/ft_atoi.c
+++ mandatory/ft_atoi.c
@@ -6,19 +6,11 @@
 /*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 23:14:26 by houamrha          #+#    #+#             */
-/*   Updated: 2024/03/10 12:21:07 by houamrha         ###   ########.fr       */
+/*   Updated: 2024/03/10 12:22:41 by houamrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-int	is_space(char c)
-{
-	if (c == ' ' || c == '\n' || c == '\t'
-		|| c == '\v' || c == '\f' || c == '\r')
-		return (1);
-	return (0);
-}
 
 int	ft_atoi(const char *str)
 {
@@ -27,8 +19,6 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	res = 0;
-	while (is_space(str[i]))
-		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = (res * 10) + (str[i] - '0');
