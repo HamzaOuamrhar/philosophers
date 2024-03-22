84615a1c236a32360998d53ded16361f3440932b
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-03-22 16:04:23 +0000

    1337

---
+++ philo/philo.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 23:13:07 by houamrha          #+#    #+#             */
+/*   Updated: 2024/03/22 13:39:31 by houamrha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	check_for_die(t_data *data)
+{
+	int	i;
+
+	while (!data->end)
+	{
+		i = 0;
+		while (!data->end && i < data->n_filo)
+		{
+			if ((data->philos[i].meals_eaten != 0
+					&& (get_time()
+						- data->philos[i].last_meal_time > data->t_die))
+				|| (data->philos[i].meals_eaten == 0
+					&& (get_time() - data->start > data->t_die)))
+			{
+				printf("[%ld] %d died\n", (get_time() - data->start),
+					data->philos[i].id);
+				data->end = 1;
+			}
+			i++;
+		}
+	}
+}
+
+int	init_philos(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->n_filo)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].full = 0;
+		data->philos[i].meals_eaten = 0;
+		if (pthread_create(&(data->philos[i].philo), NULL, &thread_handler,
+				&(data->philos[i])) != 0)
+			return (0);
+		i++;
+	}
+	data->ready = 1;
+	data->start = get_time();
+	check_for_die(data);
+	i = 0;
+	while (i < data->n_filo)
+	{
+		if (pthread_join(data->philos[i].philo, NULL) != 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	init_data(t_data *data)
+{
+	int	i;
+
+	data->ready = 0;
+	data->end = 0;
+	i = 0;
+	while (i < data->n_filo)
+	{
+		data->philos[i].data = data;
+		if ((i + 1) % 2 == 0)
+		{
+			data->philos[i].left_fork = &(data->forks[i]);
+			data->philos[i].right_fork = &(data->forks[(i + 1) % data->n_filo]);
+		}
+		else
+		{
+			data->philos[i].left_fork = &(data->forks[(i + 1) % data->n_filo]);
+			data->philos[i].right_fork = &(data->forks[i]);
+		}
+		i++;
+	}
+	if (!init_philos(data))
+		return (0);
+	return (1);
+}
+
+int	init(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->n_forks)
+	{
+		data->forks[i].id = i;
+		if (pthread_mutex_init(&(data->forks[i].fork), NULL) != 0)
+			return (0);
+		i++;
+	}
+	if (!init_data(data))
+		return (0);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (argc < 5 || argc > 6)
+	{
+		printf("Number of arguments not valide!\n");
+		return (1);
+	}
+	if (!parse(argc, argv, &data))
+	{
+		printf("Some arguments arn't valide!\n");
+		return (1);
+	}
+	if (!init(&data))
+	{
+		printf("Error!\n");
+		return (1);
+	}
+	return (0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 23:14:14 by houamrha          #+#    #+#             */
+/*   Updated: 2024/03/22 13:42:22 by houamrha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef struct s_philo	t_philo;
+typedef struct s_data	t_data;
+typedef struct s_fork	t_fork;
+
+struct s_fork
+{
+	pthread_mutex_t	fork;
+	int				id;
+};
+
+struct s_philo
+{
+	pthread_t	philo;
+	int			id;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	t_data		*data;
+	int			meals_eaten;
+	long		last_meal_time;
+	int			full;
+};
+
+struct s_data
+{
+	t_philo	philos[200];
+	t_fork	forks[200];
+	int		n_filo;
+	int		n_forks;
+	int		t_die;
+	int		t_eat;
+	int		t_sleep;
+	int		n_must_eat;
+	int		ready;
+	int		end;
+	long	start;
+};
+
+int		ft_atoi(const char *str);
+int		are_valide_args(int argc, char **argv);
+long	get_time(void);
+void	precise_usleep(long mls);
+int		ft_strcmp(char *s1, char *s2);
+void	write_logs(char *s, t_philo *philo);
+void	*thread_handler(void *p);
+void	thinking(t_philo *philo);
+void	sleeping(t_philo *philo);
+void	eating(t_philo *philo);
+int		parse(int argc, char **argv, t_data *data);
+
+#endif
